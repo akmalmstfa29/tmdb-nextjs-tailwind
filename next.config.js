0392fb85ccee237e35bf92fdcd1b4c827b7cfe22
Akmal Mstfa <akmalmstfa29@gmail.com>
2023-07-08 13:16:43 +0700

    Uncomment required next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
     dest: "public",
-    // register: true,
-    // skipWaiting: true,
+    register: true,
+    skipWaiting: true,
     swSrc: 'service-worker.js',
     disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = withPWA({
-  // reactStrictMode: true,
-  // swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['image.tmdb.org']
   }
